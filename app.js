20a3cbd4a46ad0fca639e03e79ce45623711651d
Richard Henry <r.henry1989@gmail.com>
2016-02-06 18:11:01 +0000

    ejs and bundel js setup

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 var unique = require('uniq');
 
+var expressLayouts = require('express-ejs-layouts');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-  res.send('Bye World');
+  res.render('index');
 });
 
 app.listen('3000', function() {
