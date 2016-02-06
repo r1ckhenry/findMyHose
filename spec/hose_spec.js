71f2ff6e1f6aedcccd0766471476b0cae98a0101
Richard Henry <r.henry1989@gmail.com>
2016-02-06 18:29:26 +0000

    hose tests running

---
+++ spec/hose_spec.js
@@ -0,0 +1,25 @@
+require('../domain/hose.js');
+
+describe('A Hose', function() {
+
+  it('should have a length', function() {
+    var hose = new Hose(10);
+    expect(hose.length).toBe(10);
+
+  });
+
+  it('should have colors', function() {
+    var hose = new Hose(null, ['Red', 'Green', 'Blue']);
+    expect(hose.colors.length).toBe(3);
+    expect(hose.colors[0]).toBe('Red');
+    expect(hose.colors[1]).toBe('Green');
+    expect(hose.colors[2]).toBe('Blue');
+  });
+
+  it('should have a description', function() {
+    var hose = new Hose(null, null, 'More bang for your buck!');
+    expect(hose.description).toBe('More bang for your buck!');
+
+  });
+
+});
